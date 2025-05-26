4a117a939f658e1e9bc7178112231884bdf057ee
sazypapi <zedekedochie@gmail.com>
2025-05-26 11:53:56 +0100

    More Commits

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -14,7 +14,7 @@ function Navbar() {
         <Suspense>
           <NavSearch />
         </Suspense>
-        <div className="flex gap-4 items-center mt-8 sm:mt-0" >
+        <div className="flex gap-4 items-center mt-2" >
           <CartButton />
           <DarkMode />
           <LinksDropdown />
