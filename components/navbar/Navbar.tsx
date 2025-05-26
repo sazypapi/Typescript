2c9ed7b89bd8d2be8a20b4574b34316ff25bcf2d
sazypapi <zedekedochie@gmail.com>
2025-05-26 11:56:36 +0100

    More navar edits

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -9,12 +9,12 @@ import LinksDropdown from "./LinksDropdown";
 function Navbar() {
   return (
     <nav className="border-b ">
-      <Containers className="flex flex-col space-y-2 sm:flex-row sm:justify-between sm:items-center flex-wrap py-8 ">
+      <Containers className="flex flex-col space-y-3 sm:flex-row sm:justify-between sm:items-center flex-wrap py-8 ">
         <Logo />
         <Suspense>
           <NavSearch />
         </Suspense>
-        <div className="flex gap-4 items-center mt-2" >
+        <div className="flex gap-4 items-center" >
           <CartButton />
           <DarkMode />
           <LinksDropdown />
